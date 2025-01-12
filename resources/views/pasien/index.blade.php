76f2057f22c09a1b71c2475d6e76ca3d46059481
arizalnurrohman <lawlietsan1302@gmail.com>
2025-01-12 16:53:50 +0700

    add other

---
+++ resources/views/pasien/index.blade.php
@@ -0,0 +1,65 @@
+@extends('layout.master')
+@section('add-css')
+
+@endsection
+@section('content')
+<div class="row">
+    <div class="col-sm-12">
+        <div class="card">
+            <div class="card-header d-flex justify-content-between">
+                <div class="header-title">
+                    <h4 class="card-title">Data Pengguna</h4>
+                </div>
+            </div>
+            <div class="card-body">
+                <p class="mb-3">Images in Bootstrap are made responsive with <code>.img-fluid</code>. <code>max-width: 100%;</code> and
+                    <code>height: auto;</code> are applied to the image so that it scales with the parent element.
+                </p>
+                <div class="table-responsive">
+                    <table id="datatable" class="table table-striped" data-toggle="data-table">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Name</th>
+                                <th>Position</th>
+                                <th>Office</th>
+                                <th>Age</th>
+                                <th>Start date</th>
+                                <th>Salary</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($pasien as $key=>$residen)
+                            <tr>
+                                <td>1</td>
+                                <td>Tiger Nixon</td>
+                                <td>System Architect</td>
+                                <td>Edinburgh</td>
+                                <td>61</td>
+                                <td>2011/04/25</td>
+                                <td>$320,800</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                        {{-- <tfoot>
+                            <tr>
+                                <th>No</th>
+                                <th>Name</th>
+                                <th>Position</th>
+                                <th>Office</th>
+                                <th>Age</th>
+                                <th>Start date</th>
+                                <th>Salary</th>
+                            </tr>
+                        </tfoot> --}}
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+ </div>
+ 
+@endsection
+@section('add-js')
+
+@endsection
