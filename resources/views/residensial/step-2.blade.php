b16524a459fd9d29913a5fc4f8a09a01dea5651a
arizalnurrohman <lawlietsan1302@gmail.com>
2025-01-25 06:39:03 +0700

    w

--- resources/views/residensial/step-2.blade.php
+++ resources/views/residensial/step-2.blade.php
@@ -18,117 +18,86 @@
                             <option value="{{ $id }}">{{ $pasienx->nama_depan." ".$pasienx->nama_belakang }}</option>
                         @endforeach
                     </select>
-                    <div class="input-group-btn"><button type="submit" class="btn btn-primary">Tambah</button></div>
+                    <div class="input-group-btn"><button type="button" class="btn btn-primary">Pilih Pasien</button></div>
                 </div>
             </div>
         </div>
         <div class="col-md-6">
             <div class="form-group">
                 <label class="form-label">NIK: *</label>
-                <input type="text" class="form-control" name="lname" placeholder="NIK" />
+                <input type="text" class="form-control" name="nik" placeholder="NIK" />
             </div>
         </div>
         <div class="col-md-6">
             <div class="form-group">
                 <label class="form-label">NO KK: *</label>
-                <input type="text" class="form-control" name="phno" placeholder="NO KK." />
+                <input type="text" class="form-control" name="nokk" placeholder="NO KK." />
             </div>
         </div>
         <div class="col-md-6">
             <div class="form-group">
                 <label class="form-label">Tempat Lahir: *</label>
-                <input type="text" class="form-control" name="phno_2" placeholder="Tempat Lahir." />
+                <input type="text" class="form-control" name="tmp_lahir" placeholder="Tempat Lahir." />
             </div>
         </div>
         <div class="col-md-6">
             <div class="form-group">
                 <label class="form-label">Tanggal Lahir: *</label>
-                <input type="text" class="form-control" name="phno_2" placeholder="Tanggal Lahir." />
+                <input type="text" class="form-control" name="tgl_lahir" placeholder="Tanggal Lahir." />
             </div>
         </div>
         <div class="col-md-12">
             <div class="form-group">
                 <label class="form-label">Usia: *</label>
-                <input type="text" class="form-control" name="phno_2" placeholder="Usia." />
+                <input type="text" class="form-control" name="usia" placeholder="Usia." />
             </div>
         </div>
         <div class="col-md-6">
             <div class="form-group">
                 <label class="form-label">Provinsi KTP: *</label>
-                <select class="form-select" data-trigger name="choices-single-default" id="choices-single-default">
-                    <option value="">Pilih Provinsi</option>
-                    @foreach($provinsi as $provinsix)
-                        <option value="{{$provinsix->id}}">{{$provinsix->provinsi}}</option>
-                    @endforeach
-                </select>
+                <input type="text" class="form-control" name="provinsi" placeholder="Provinsi." />
             </div>
         </div>
         <div class="col-md-6">
             <div class="form-group">
                 <label class="form-label">Kab/Kota KTP: *</label>
-                <select class="form-select" data-trigger name="choices-single-default" id="choices-single-default">
-                    <option value="">Pilih Kabupaten</option>
-                    @foreach($kabupaten as $kabupatenx)
-                        <option value="{{$kabupatenx->id}}">{{$kabupatenx->kabupaten_kota}}</option>
-                    @endforeach
-                </select>
+                <input type="text" class="form-control" name="kabupaten" placeholder="Kabupaten / Kota." />
             </div>
         </div>
         <div class="col-md-6">
             <div class="form-group">
                 <label class="form-label">Kecamatan KTP: *</label>
-                <select class="form-select" data-trigger name="choices-single-default" id="choices-single-default">
-                    <option value="">Pilih Kecamatan</option>
-                    @foreach($kecamatan as $kecamatanx)
-                        <option value="{{$kecamatanx->id}}">{{$kecamatanx->kecamatan}}</option>
-                    @endforeach
-                </select>
+                <input type="text" class="form-control" name="kecamatan" placeholder="Kecamatan." />
             </div>
         </div>
         <div class="col-md-6">
             <div class="form-group">
                 <label class="form-label">Kelurahan KTP: *</label>
-                <input type="text" class="form-control" name="phno_2" placeholder="Kelurahan KTP." />
+                <input type="text" class="form-control" name="kelurahan" placeholder="Kelurahan KTP." />
             </div>
         </div>
         <div class="col-md-6">
             <div class="form-group">
                 <label class="form-label">Alamat KTP: *</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
+                <textarea class="form-control" name="alamat" rows="5"></textarea>
             </div>
         </div>
         <div class="col-md-6">
             <div class="form-group">
                 <label class="form-label">Alamat Domisili: *</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
-                <div class="form-check d-block">
-                    <input class="form-check-input" type="checkbox" value="" id="checkedcheck" checked>
-                    <label class="form-check-label" for="checkedcheck">
-                        Sama dengan Alamat KTP
-                    </label>
-                </div>
+                <textarea class="form-control" name="domisili" rows="5"></textarea>
             </div>
         </div>
         <div class="col-md-6">
             <div class="form-group">
                 <label class="form-label">Agama: *</label>
-                <select class="form-select" data-trigger name="choices-single-default" id="choices-single-default">
-                    <option value="">Pilih Agama</option>
-                    @foreach($agama as $agamax)
-                        <option value="{{$agamax->id}}">{{$agamax->agama}}</option>
-                    @endforeach
-                </select>
+                <input type="text" class="form-control" name="agama" placeholder="Agama." />
             </div>
         </div>
         <div class="col-md-6">
             <div class="form-group">
                 <label class="form-label">Pendidikan: *</label>
-                <select class="form-select" data-trigger name="choices-single-default" id="choices-single-default">
-                    <option value="">Pilih Pendidikan</option>
-                    @foreach($pendidikan as $pendidikanx)
-                        <option value="{{$pendidikanx->id}}">{{$pendidikanx->pendidikan}}</option>
-                    @endforeach
-                </select>
+                <input type="text" class="form-control" name="pendidikan" placeholder="Pendidikan." />
             </div>
         </div>
         
